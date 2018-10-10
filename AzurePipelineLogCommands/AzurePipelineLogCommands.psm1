9ff03cc7f4876a72d92aa86e5b3f770954ea7a9a
Joshua Shearer <Rychard@users.noreply.github.com>
2018-10-10 17:57:23 -0400

    Create module infrastructure

---
+++ AzurePipelineLogCommands/AzurePipelineLogCommands.psm1
@@ -0,0 +1,18 @@
+Set-StrictMode -Version Latest
+
+#Get public and private function definition files.
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+
+#Dot source the files
+@($Public + $Private) | ForEach-Object {
+    $fileToImport = $_
+    Try {
+        . $fileToImport.FullName
+    }
+    Catch {
+        Write-Error -Message "Failed to import function $($fileToImport.FullName): $_"
+    }
+}
+
+Export-ModuleMember -Function $Public.BaseName
